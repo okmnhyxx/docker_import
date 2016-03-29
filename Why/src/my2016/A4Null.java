b674bb27a33cf919936ded34070612f667566e8b
emi <emi.lu@cloudtopcn.com>
2016-03-29 19:53:44 +0800

    C7Son

---
+++ Why/src/my2016/A4Null.java
@@ -0,0 +1,22 @@
+package my2016;
+
+/**
+ * Created by emi on 2016/3/14.
+ */
+public class A4Null {
+
+    public static void main(String[] args) {
+        A4Null a = new A4Null();
+        a.testNullStr();
+    }
+
+    private void testNullStr() {
+
+        String a = null;
+        System.out.println(a);
+
+        StringBuffer sb = new StringBuffer();
+
+
+    }
+}
