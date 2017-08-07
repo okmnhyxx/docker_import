3bbea05b06cbf5644ff3699fc08bd99f2d5bc634
yunni <yunni@nef.com>
2017-08-07 23:55:17 +0800

    add gitignore

---
+++ Why/src/my2017/A8Singleton.java
@@ -0,0 +1,15 @@
+package my2017;
+
+/**
+ * Created by yunni on 2017/7/23.
+ * http://www.cnblogs.com/yinxiaoqiexuxing/p/5605338.html
+ *
+ */
+public class A8Singleton {
+
+
+    public static void main(String[] args) {
+        A8Singleton a = new A8Singleton();
+        A8Singleton.class.getName();
+    }
+}
